7dd394bc3f83bb5ea21f6bbf2f6077661374907d
Yuki-Narita <18kmj27@ms.dendai.ac.jp>
2019-02-11 03:08:57 +0900

    complete voronoi  planner

--- src/voronoi_test.cpp
+++ src/voronoi_test.cpp
@@ -1,6 +1,7 @@
 #include <ros/ros.h>
 #include <costmap_2d/costmap_2d_ros.h>
 #include <voronoi_planner/planner_core.h>
+#include <geometry_msgs/PoseStamped.h>
 
 int main(int argc, char** argv)
 {
@@ -8,9 +9,33 @@ int main(int argc, char** argv)
   tf::TransformListener tf(ros::Duration(10));
   costmap_2d::Costmap2DROS lcr("costmap", tf);
 
-  VoronoiPlanner vp("test_map", &lcr, "map");
+  std::string name = "test_map";
+
+  voronoi_planner::VoronoiPlanner vp;
+
+  vp.initialize(name, &lcr);
+
+  geometry_msgs::PoseStamped start;
+  geometry_msgs::PoseStamped goal;
+  std::vector<geometry_msgs::PoseStamped> plan;
+
+  start.header.frame_id = "robot1/map";
+  goal.header.frame_id = "robot1/map";
+
+  goal.pose.position.x = 5.0;
+
+  //vp.makePlan(start,goal,plan);
+
+  //std::cout << "frame_id :: " << lcr.getGlobalFrameID() << std::endl;
+  ros::spinOnce();
+
+  //makePlan no ato ni costmap no kousin ga hairuto error
+
+  //voronoi_planner no cmakelist kaikikae dynamic_voronoi no tokoro
 
   while(ros::ok()){
+    //ros::spinOnce();
+    vp.makePlan(start,goal,plan);
     ros::spinOnce();
   }
 
